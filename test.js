969809aeb10a55c71eb3efe77011fdaed98bc3cb
changhyeokyoo <yoiue4231@kakao.com>
2023-03-13 15:54:57 +0900

    수정기능 추가

--- test.js
+++ test.js
@@ -2,7 +2,13 @@ const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', () => {
   console.log('클릭');
 });
+
 const $btnDel = document.getElementById('btnDel');
 $btnDel.addEventListener('click', () => {
   console.log('삭제');
+});
+
+const $btnUpdate = document.getElementById('btnUpdate');
+$btnUpdate.addEventListener('click', () => {
+  console.log('수정');
 });
